506ba9e719a2484d7cac6d840b3ef393082cfac3
zouh9907 <zouh@rose-hulman.edu>
2017-11-29 09:03:18 -0500

    finish 1 2 3 4 5

--- src/m4_functions_vs_methods.py
+++ src/m4_functions_vs_methods.py
@@ -4,12 +4,12 @@ Demonstrates using (calling) FUNCTIONS and using (calling) METHODS:
   -- how they differ.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Huiruo Zou.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   READ this comment, ASKING QUESTIONS as needed to understand it.
 #
 #   For objects that are CONSTRUCTED, we use the DOT notation
@@ -80,6 +80,10 @@ def main():
     turtle = rg.SimpleTurtle('square')
     draw_many_squares(turtle, 3, 75, 15)
     turtle3()
+
+    try_methods()
+    try_functions()
+    try_methods_and_functions()
 
     # When the TODOs ask you to test YOUR code, put YOUR tests here:
 
@@ -152,6 +156,15 @@ def turtle3():
     maja = rg.SimpleTurtle()
     maja.pen = rg.Pen('green', 10)
     maja.paint_bucket = rg.PaintBucket('black')
+    """
+    Constructs a SimpleTurtle and sets its   pen   to a new rg.Pen
+    that is 'brown' with thickness 5.
+    Then makes the SimpleTurtle move as follows (in the order listed):
+      -- forward   150 units
+      -- left       90 degrees
+      -- forward    50 units
+      -- backward  100 units
+    """
 
     maja.right(135)
     maja.forward(300)
@@ -162,23 +175,25 @@ def turtle3():
 
 
 def try_methods():
-    """
-    Constructs a SimpleTurtle and sets its   pen   to a new rg.Pen
-    that is 'brown' with thickness 5.
-    Then makes the SimpleTurtle move as follows (in the order listed):
-      -- forward   150 units
-      -- left       90 degrees
-      -- forward    50 units
-      -- backward  100 units
-    """
+    apple = rg.SimpleTurtle()
+    apple.pen = rg.Pen('brown',5)
+
+    apple.forward(150)
+    apple.left(90)
+    apple.forward(50)
+    apple.backward(100)
     ####################################################################
-    # TODO: 3. Implement this function, per its doc-string above.
+    # DONE: 3. Implement this function, per its doc-string above.
     #    Put a statement in   main   to test this function
     #    (by calling this function).
     ####################################################################
 
 
 def try_functions():
+    jump_and_move_turtle(200, 100, 300, 30)
+    jump_and_move_turtle(100, 200, 0, 0)
+    jump_and_move_turtle(-50, 50, 100, 100)
+
     """
     Causes several SimpleTurtles to do the following:
      -- One jumps to (200, 100), then moves (while drawing) to (300, 30)
@@ -186,7 +201,7 @@ def try_functions():
      -- One jumps to (-50, 50), then moves (while drawing) to (100, 100)
     """
     ####################################################################
-    # TODO: 4. Implement this function, per its doc-string above.
+    # DONE: 4. Implement this function, per its doc-string above.
     #    Put a statement in   main   to test this function
     #    (by calling this function).  IMPORTANT, IMPORTANT, IMPORTANT:
     #    Keep reading the rest of this TO DO before doing the above!
@@ -200,6 +215,30 @@ def try_functions():
 
 
 def try_methods_and_functions():
+    amber = rg.SimpleTurtle()
+    amber.pen = rg.Pen('blue',5)
+
+    amber.backward(150)
+    amber.speed = 1
+    draw_many_squares(amber, 2, 100, 30)
+
+    amber.speed = 1
+    amber.pen = rg.Pen('red',5)
+    draw_many_squares(amber, 5, 50, 15)
+
+    amber.speed = 100
+    amber.pen = rg.Pen('red',35)
+    draw_many_squares(amber, 8, 300, 60)
+
+    amber.pen = rg.Pen('black',3)
+
+    amber.backward(200)
+    amber.draw_circle(30)
+    amber.draw_square(50)
+
+
+
+
     """
     Constructs a SimpleTurtle and sets its   pen  to a new rg.Pen
     that is 'blue' with thickness 5.
@@ -232,7 +271,7 @@ def try_methods_and_functions():
       8. Draw a SQUARE whose sides are each of length 50.
     """
     ####################################################################
-    # TODO: 5. Implement this function, per its doc-string above.
+    # DONE: 5. Implement this function, per its doc-string above.
     #    Put a statement in   main   to test this function
     #    (by calling this function).  IMPORTANT, IMPORTANT, IMPORTANT:
     #    Keep reading the rest of this TO DO before doing the above!
